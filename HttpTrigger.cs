1451659ea7c051d12c7eab6d9edc2332c1fa7950
Salvatore Sammarco <microjmpr@gmail.com>
2023-10-28 12:15:09 +0200

    cache invalidation on writing operations

--- HttpTrigger.cs
+++ HttpTrigger.cs
@@ -4,13 +4,13 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.Cosmos;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.IO;
 using System.Linq;
 using System;
 using Newtonsoft.Json;
 using newshub.types;
+using newshub.functions.utils;
 
 namespace newshub.functions;
 
@@ -50,6 +50,9 @@ public static class HttpTrigger
             var container = CosmosClientManager.Instance.GetContainer("newshub", "articles");
             newArticle.Id = Guid.NewGuid().ToString();
             var response = await container.CreateItemAsync(newArticle, new PartitionKey(newArticle.Id));
+
+            CacheManager.Invalidate();
+
             return new OkObjectResult(response.Resource);
         }
         catch (Exception e)
@@ -68,6 +71,9 @@ public static class HttpTrigger
         try {
             var container = CosmosClientManager.Instance.GetContainer("newshub", "articles");
             var response = await container.ReplaceItemAsync(updatedArticle, updatedArticle.Id, new PartitionKey(updatedArticle.Id));
+
+            CacheManager.Invalidate();
+
             return new OkObjectResult(response.Resource);
         }
         catch (Exception e)
@@ -77,26 +83,23 @@ public static class HttpTrigger
         }
     }
 
-#region Delete
     [FunctionName("DeleteArticle")]
     public static async Task<IActionResult> DeleteArticle(
-        [HttpTrigger(AuthorizationLevel.Function, "delete", Route = "articles/delete/{id}")] HttpRequest req, string id)
+        [HttpTrigger(AuthorizationLevel.Function, "delete", Route = "articles/delete/{id}")] HttpRequest req, ILogger log, string id)
     {
-        using CosmosClient cosmosClient = new(
-            connectionString: Environment.GetEnvironmentVariable("CONNECTION_STRING_SETTING"),
-            new CosmosClientOptions()
-            {
-                SerializerOptions = new CosmosSerializationOptions()
-                {
-                    PropertyNamingPolicy = CosmosPropertyNamingPolicy.CamelCase
-                }
-            }
-        );
-        var container = cosmosClient.GetContainer("newshub", "articles");
-        var response = await container.DeleteItemAsync<Article>(id, new PartitionKey(id));
+        try
+        {
+            var container = CosmosClientManager.Instance.GetContainer("newshub", "articles");
+            var response = await container.DeleteItemAsync<Article>(id, new PartitionKey(id));
 
-        return new OkObjectResult(response.Resource);
-    }
-#endregion
-    
+            CacheManager.Invalidate();
+
+            return new OkObjectResult(response.Resource);
+        }
+        catch (Exception e)
+        {
+            log.LogError(e.Message);
+            return new StatusCodeResult(500);
+        }
+    }    
 }
